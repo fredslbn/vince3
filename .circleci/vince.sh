571e2d5c564b3ba15b0f957ee2450148443bfc21
fredslbn <fh.silaban@gmail.com>
2023-08-19 10:35:11 +0700

    jaj

--- .circleci/vince.sh
+++ .circleci/vince.sh
@@ -243,11 +243,10 @@ START=$(date +"%s")
 	   then
 	       make -kj$(nproc --all) O=out \	       
 	       ARCH=arm64 \
-	       
 	       CC=clang \
 	       CROSS_COMPILE=aarch64-linux-gnu- \
 	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-	       LLVM=1 \
+	       #LLVM=1 \
 	       #LLVM_IAS=1 \
 	       #LD=${LINKER} \
 	       #AR=llvm-ar \
