6af7a677ef97830364e52a37aa5e61f1b807922a
fredslbn <fh.silaban@gmail.com>
2023-08-19 11:15:51 +0700

    j

--- .circleci/vince.sh
+++ .circleci/vince.sh
@@ -170,15 +170,7 @@ function cloneTC() {
 function exports() {
 	
         # Export KBUILD_COMPILER_STRING
-#        if [ -d ${KERNEL_DIR}/clang ];
-#           then
-#               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-#               export LD_LIBRARY_PATH="${KERNEL_DIR}/clang/lib:$LD_LIBRARY_PATH"
-               
-#        elif [ -d ${KERNEL_DIR}/gcc64 ];
-#           then
-#               export KBUILD_COMPILER_STRING=$("$KERNEL_DIR/gcc64"/bin/aarch64-elf-gcc --version | head -n 1)       
-               
+
         if [ -d ${KERNEL_DIR}/cosmic ];
            then
                export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/cosmic/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')        
@@ -243,36 +235,12 @@ START=$(date +"%s")
 	   then
 	       make -kj$(nproc --all) O=out \	       
 	       ARCH=arm64 \
-#	       CC=clang \
-#	       CROSS_COMPILE=aarch64-linux-gnu- \
-#	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-	       #LLVM=1 \
-	       #LLVM_IAS=1 \
-	       #LD=${LINKER} \
-	       #AR=llvm-ar \
-	       #NM=llvm-nm \
-	       #OBJCOPY=llvm-objcopy \
-	       #DIS=llvm-dis \
-	       #STRIP=llvm-strip \
-	       #READELF=llvm-readelf \
-	       #OBJSIZE=llvm-size \
-	       
 	       CC=$KERNEL_CLANG \
            CROSS_COMPILE=$KERNEL_CCOMPILE64 \
            CROSS_COMPILE_ARM32=$KERNEL_CCOMPILE32 \
-#           CLANG_TRIPLE=aarch64-linux-gnu- \
-           #LD=${LINKER} \
-#           LLVM=1 \
-           #LLVM_IAS=1 \
-           #AR=llvm-ar \
-           #NM=llvm-nm \
-           #OBJCOPY=llvm-objcopy \
-           #OBJDUMP=llvm-objdump \
-           #STRIP=llvm-strip \
-	       
+           CLANG_TRIPLE=aarch64-linux-gnu- \
 	       V=$VERBOSE 2>&1 | tee error.log
-	       
-	       
+	      
 	elif [ -d ${KERNEL_DIR}/cosmic ];
 	   then
 	       make -j$(nproc --all) O=out \
